56bd6ca0ca17f23bee52e962653d564f7cbfce5e
Doan NGO <ngodoan.utc@gmail.com>
2024-05-27 16:11:04 +0700

    feat: Call repository to load user data by userId from database.
    fix: Format code
    
    fix: Format code
    
    fix: Add update user data to UI

--- app/src/main/java/com/fpt/demogit/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fpt/demogit/ui/main/MainViewModel.kt
@@ -3,8 +3,19 @@ package com.fpt.demogit.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private val repository = MainRepository()
+
     fun load(userId: String) {
         // Load user from database
+        repository.load(userId)
+        // Update UI
+        state.updateState {
+            it.copy(
+                isLoading = false,
+                user = repository.getUser(userId)
+            )
+        }
     }
     // TODO: Implement the ViewModel
 }
