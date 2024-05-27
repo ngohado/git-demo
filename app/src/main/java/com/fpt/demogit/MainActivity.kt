0e21955df2a7c16eee64b7937c9fae5ea15c15d0
Doan NGO <ngodoan.utc@gmail.com>
2024-05-27 14:39:32 +0700

    feat: Add main feature

---
+++ app/src/main/java/com/fpt/demogit/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fpt.demogit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fpt.demogit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/fpt/demogit/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.fpt.demogit.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
